3168d1d6eb098185a165ed1a51a9e8a9667a2742
PedroRamosRguez <p.ramos1990@gmail.com>
2018-09-04 18:17:49 +0000

    usando promesas para la memoria total y la libre

--- getMem.js
+++ getMem.js
@@ -1,22 +1,20 @@
-exec = require('child_process').exec
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-module.exports = function getMem(){
-  setInterval(function(){
-    var memoriaTotal;
-    child1 = exec("awk '/MemTotal/ { print $2/1048576 }' /proc/meminfo",function(error, stdout, stderr){
-      memoriaTotal = stdout;
-    });
-    
-    child2 = exec("awk '/MemFree/ {print $2/1048576 }' /proc/meminfo", function(errror, stdout, stderr){
-      memoriaLibre = stdout;
-      memoriaUsada = memoriaTotal - memoriaLibre;
-      porcentajeMemUsada = Math.round(memoriaUsada*100/memoriaTotal);
-      porcentajeMemLibre = 100 - porcentajeMemUsada;
-      console.log('memoria total en gb: '+memoriaTotal);
-      console.log('memoria libre en gb: '+memoriaLibre);
-      console.log('memoria usada en gb: '+memoriaUsada);
-      console.log('porcentaje memoria usada: '+porcentajeMemUsada);
-      console.log('porcentaje memoria libre: '+porcentajeMemLibre); 
-    });
-  },5000)
-};
+async function getMemTotal(){
+  const { stdout, stderr } = await exec("awk '/MemTotal/ { print $2/1048576 }' /proc/meminfo");
+  if(stderr){
+    console.log(`error ${stderr}`);
+  }
+  return stdout;
+}
+
+async function getMemLibre(){
+  const {stdout, stderr } = await exec("awk '/MemFree/ {print $2/1048576 }' /proc/meminfo");
+  if(stderr){
+    console.log(`error ${stderr}`);
+  }
+  return stdout;
+}
+
+module.exports = { getMemTotal, getMemLibre }
