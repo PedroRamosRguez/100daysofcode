812567e08d060531b7467550276741ce367c74b5
PedroRamosRguez <p.ramos1990@gmail.com>
2018-09-15 22:01:50 +0100

    añadida sidebar, añadida awesome-fonts para los iconos y preparando las cards para añadir la informacion de las medidas

--- rasp-monitoring/src/App.vue
+++ rasp-monitoring/src/App.vue
@@ -1,14 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cpu">Cpu</router-link>|
-      <router-link to="/mem">Memory</router-link>|
-      <router-link to="/storage">Storage</router-link>|
-      <router-link to="/temp">Temperature</router-link>|
-      <router-link to="/about">About</router-link>
+    <div class="row wrapper">
+      <aside class="col-12 col-sm-2 bg-dark">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark align-items-start flex-sm-column flex-row">
+          <a class="navbar-brand mx-auto" href="#"><i class="fab fa-vuejs fa-fw"></i> RaspMonitoring</a>
+          <a href class="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
+            <span class="navbar-toggler-icon"></span>
+          </a>
+          <div class="collapse navbar-collapse sidebar">
+            <ul class="flex-column navbar-nav w-100 justify-content-between">
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-microchip fa-fw"></i> <span class=""> Cpu</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#m1" data-toggle="collapse" data-target="#m1" aria-expanded="false">
+                 <i class="fas fa-thermometer-three-quarters fa-fw"></i> <span class="">Temperatura</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-memory"></i> <span class="">  Memoria</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-hdd fa-fw"></i> <span class="">  Almacenamiento</span></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </aside>
+      <main class="col bg-faded py-3">
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/cpu">Cpu</router-link>|
+          <router-link to="/mem">Memory</router-link>|
+          <router-link to="/storage">Storage</router-link>|
+          <router-link to="/temp">Temperature</router-link>|
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
+      </main>
     </div>
-    <router-view/>
   </div>
 </template>
 <style>
@@ -31,4 +61,21 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+
+body, html, .container-fluid {
+    height: 100%;
+}
+
+.wrapper {
+    min-height: 100vh;
+    max-height: 100%;
+}
+
+main {
+    max-height: 100%;
+    overflow-y: auto;
+}
+
+
 </style>
--- rasp-monitoring/src/views/Home.vue
+++ rasp-monitoring/src/views/Home.vue
@@ -1,39 +1,103 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png"><br/>
-    Esto es la  pagina de inicio...
-    <p>Uso de la Cpu:{{usoCpu}}</p>
-    <p>Temperatura de la Cpu:{{temperaturaCpu}}</p>
-    <p>Temperatura de la Gpu:{{temperaturaGpu}}</p>
-    <p>Cantidad de memoria RAM total:{{memTotal}}</p>
-    <p>Cantidad de memoria RAM libre:{{memLibre}}</p>
-    <p>Cantidad de memoria RAM usada:{{memUsada}}</p>
-    <p>Cantidad de memoria RAM usada en Porcentaje:{{porcentajeMemLibre}} %</p>
-    <p>Cantidad de memoria RAM usada en porcentaje:{{porcentajeMemUsada}} %</p>
-    <table class="table">
-      <thead>
-        <tr>
-	  <th scope = "col">Sistema Ficheros</th>
-	  <th scope = "col">Tamaño</th>
-	  <th scope = "col">Espacio Libre</th>
-	  <th scope = "col">Espacio Usado</th>
-	  <th scope = "col">Porcentaje</th>
-        </tr>
-      </thead>
-      <tbody>
-       <template v-for="item in storage">
-         <tr>
-           <td scope="row">{{item.sistFicheros}}</td>
-           <td scope="row">{{item.tamanio}}</td>
-           <td scope="row">{{item.espacioLibre}}</td>
-           <td scope="row">{{item.espacioUsado}}</td>
-           <td scope="row">{{item.porcentaje}}</td>
-         </tr>
-       </template>
-      </tbody>
-    </table>
+  <div class="container-fluid">
+    <div class="home">
+      <!-- <img alt="Vue logo" src="../assets/logo.png"><br/>
+      Esto es la  pagina de inicio... -->
+      <div class = "row">
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Uso de la Cpu</div>
+            <div class="card-block">{{usoCpu}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Temperatura de la Cpu</div>
+            <div class="card-block">{{temperaturaCpu}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Temperatura de la Gpu</div>
+            <div class="card blockl">{{temperaturaGpu}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <div class = "row">
+        <div class = "col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Cantidad de memoria RAM total</div>
+            <div class="card blockl">{{memTotal}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+        <div class = "col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Cantidad de memoria RAM libre</div>
+            <div class="card blockl">{{memLibre}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+        <div class = "col-sm-4">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Cantidad de memoria RAM usada</div>
+            <div class="card blockl">{{memUsada}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <div class = "row">
+        <div class = "col-sm-6">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Cantidad de memoria RAM libre en Porcentaje</div>
+            <div class="card blockl">{{porcentajeMemLibre}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+        <div class = "col-sm-6">
+          <div class="card">
+            <div class="card-header card-primary no-margin">Cantidad de memoria RAM usada en porcentaje</div>
+            <div class="card blockl">{{porcentajeMemUsada}}</div>
+            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope = "col">Sistema Ficheros</th>
+            <th scope = "col">Tamaño</th>
+            <th scope = "col">Espacio Libre</th>
+            <th scope = "col">Espacio Usado</th>
+            <th scope = "col">Porcentaje</th>
+          </tr>
+        </thead>
+        <tbody>
+        <template v-for="item in storage">
+          <tr>
+            <td scope="row">{{item.sistFicheros}}</td>
+            <td scope="row">{{item.tamanio}}</td>
+            <td scope="row">{{item.espacioLibre}}</td>
+            <td scope="row">{{item.espacioUsado}}</td>
+            <td scope="row">{{item.porcentaje}}</td>
+          </tr>
+        </template>
+        </tbody>
+      </table>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -51,7 +115,7 @@ export default {
       memUsada: 0,
       porcentajeMemLibre: 0,
       porcentajeMemUsada: 0,
-      storage : [],
+      storage: [],
     };
   },
   methods: {
@@ -122,12 +186,11 @@ export default {
     getStorage() {
       const self = this;
       setInterval(() => {
-	//self.storage = []
         axios.get('http://192.168.1.42:3000/storage')
           .then((response) => {
             console.log(response);
             console.log(response.data);
-	    self.storage = response.data
+            self.storage = response.data;
           })
           .catch((error) => {
             console.log(error);
