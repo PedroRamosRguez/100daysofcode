4a0a376e444ffe87cb4cde42051d3c4cd24cb9e2
PedroRamosRguez <p.ramos1990@gmail.com>
2018-09-18 16:06:12 +0000

    Ya se muestra la informacion en la pagina principal con las gráficas. Falta añadir algo de estilo

--- rasp-monitoring/src/App.vue
+++ rasp-monitoring/src/App.vue
@@ -3,45 +3,46 @@
     <div class="row wrapper">
       <aside class="col-12 col-sm-2 bg-dark">
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark align-items-start flex-sm-column flex-row">
-          <a class="navbar-brand mx-auto" href="#"><i class="fab fa-vuejs fa-fw"></i> RaspMonitoring</a>
+          <router-link class="navbar-brand mx-auto" to="/"><i class="fab fa-vuejs fa-fw"></i> RaspMonitoring</router-link>
           <a href class="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
             <span class="navbar-toggler-icon"></span>
           </a>
           <div class="collapse navbar-collapse sidebar">
             <ul class="flex-column navbar-nav w-100 justify-content-between">
               <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fas fa-microchip fa-fw"></i> <span class=""> Cpu</span></a>
+                <router-link class="nav-link" to="/cpu"><i class="fas fa-microchip fa-fw"></i> <span class=""> Cpu</span></router-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#m1" data-toggle="collapse" data-target="#m1" aria-expanded="false">
+                <router-link class="nav-link" to="/temp" data-toggle="collapse" data-target="#m1" aria-expanded="false">
                  <i class="fas fa-thermometer-three-quarters fa-fw"></i> <span class="">Temperatura</span>
-                </a>
+                </router-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fas fa-memory"></i> <span class="">  Memoria</span></a>
+                <router-link class="nav-link" to="/mem"><i class="fas fa-memory"></i> <span class="">  Memoria</span></router-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fas fa-hdd fa-fw"></i> <span class="">  Almacenamiento</span></a>
+                <router-link class="nav-link" to="/storage"><i class="fas fa-hdd fa-fw"></i> <span class="">  Almacenamiento</span></router-link>
               </li>
             </ul>
           </div>
         </nav>
       </aside>
       <main class="col bg-faded py-3">
-        <div id="nav">
+        <!--<div id="nav">
           <router-link to="/">Home</router-link> |
           <router-link to="/cpu">Cpu</router-link>|
           <router-link to="/mem">Memory</router-link>|
           <router-link to="/storage">Storage</router-link>|
           <router-link to="/temp">Temperature</router-link>|
           <router-link to="/about">About</router-link>
-        </div>
+        </div>-->
         <router-view/>
       </main>
     </div>
   </div>
 </template>
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -76,6 +77,7 @@ main {
     max-height: 100%;
     overflow-y: auto;
 }
-
-
+.chart{
+    background-color: #343A40;
+}
 </style>
--- rasp-monitoring/src/views/Home.vue
+++ rasp-monitoring/src/views/Home.vue
@@ -27,72 +27,44 @@
       <br/>
       <br/>
       <div class = "row">
-        <div class = "col-sm-4">
+        <div class = "col-sm-6 mx-auto">
           <div class="card">
-            <div class="card-header card-primary no-margin">Cantidad de memoria RAM total</div>
-            <div class="card blockl">{{memTotal}}</div>
-            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+            <div class="card-header card-primary no-margin">Memoria RAM (Gb)</div>
+            <div class="card blockl memLibre">
+              <pie-chart :data="[['Mem. Libre',memLibre],['Mem.Usada',memUsada]]"/>
+            </div>
+            Cantidad Memoria Ram Total {{memTotal}}<br/>
+            Cantidad Memoria Ram Libre {{memLibre}}<br/>
+            Cantidad Memoria Ram Usada {{memUsada}}<br/>
           </div>
         </div>
-        <div class = "col-sm-4">
+        <div class = "col-sm-6 mx-auto">
           <div class="card">
-            <div class="card-header card-primary no-margin">Cantidad de memoria RAM libre</div>
-            <div class="card blockl">{{memLibre}}</div>
-            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
-          </div>
-        </div>
-        <div class = "col-sm-4">
-          <div class="card">
-            <div class="card-header card-primary no-margin">Cantidad de memoria RAM usada</div>
-            <div class="card blockl">{{memUsada}}</div>
-            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
-          </div>
-        </div>
-      </div>
-      <br/>
-      <br/>
-      <br/>
-      <div class = "row">
-        <div class = "col-sm-6">
-          <div class="card">
-            <div class="card-header card-primary no-margin">Cantidad de memoria RAM libre en Porcentaje</div>
-            <div class="card blockl">{{porcentajeMemLibre}}</div>
-            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
-          </div>
-        </div>
-        <div class = "col-sm-6">
-          <div class="card">
-            <div class="card-header card-primary no-margin">Cantidad de memoria RAM usada en porcentaje</div>
-            <div class="card blockl">{{porcentajeMemUsada}}</div>
-            <img src="https://camo.githubusercontent.com/8299979afd2e8a2fce33e9d68e89d2f1c3e5ecf3/687474703a2f2f63646e2e6d616b657573656f662e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031322f30322f7261737062657272795f70695f6c6f676f2e706e673f366632356337">
+            <div class="card-header card-primary no-margin">Porcentaje memoria RAM</div>
+            <div class="card blockl">
+              <pie-chart :data="[['Mem. Libre',porcentajeMemLibre],['Mem.Usada',porcentajeMemUsada]]" :colors="['#AF4C37','#0B76CA']"/>
+            </div>
+            Porcentaje Memoria Ram Libre {{porcentajeMemLibre}} %<br/>
+            <br/>
+            Porcentaje Memoria Ram Usada {{porcentajeMemUsada}} %<br/>
           </div>
         </div>
       </div>
       <br/>
       <br/>
       <br/>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope = "col">Sistema Ficheros</th>
-            <th scope = "col">Tamaño</th>
-            <th scope = "col">Espacio Libre</th>
-            <th scope = "col">Espacio Usado</th>
-            <th scope = "col">Porcentaje</th>
-          </tr>
-        </thead>
-        <tbody>
+      <div class="row">
         <template v-for="item in storage">
-          <tr>
-            <td scope="row">{{item.sistFicheros}}</td>
-            <td scope="row">{{item.tamanio}}</td>
-            <td scope="row">{{item.espacioLibre}}</td>
-            <td scope="row">{{item.espacioUsado}}</td>
-            <td scope="row">{{item.porcentaje}}</td>
-          </tr>
+          <div class = "col-sm-3">
+            <div class="card">
+              <div class="card-header card-primary no-margin">{{item.sistFicheros}}</div>
+              <div class="card blockl">
+                <pie-chart :data="[['Espacio Usado (Gb)',item.espacioUsado],['Espacio Libre (Gb)',item.espacioLibre]]"/>
+              </div>
+            </div>
+          </div>
         </template>
-        </tbody>
-      </table>
+      </div>
     </div>
   </div>
 </template>
