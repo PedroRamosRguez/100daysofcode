3198677d1caf54545049dcc906a265602cc4b1ca
PedroRamosRguez <p.ramos1990@gmail.com>
2018-09-25 17:38:08 +0000

    modificado para que envíe rutas de ficheros además de los directorios

--- src/tree.js
+++ src/tree.js
@@ -12,11 +12,12 @@ async function getTreeDirectory(){
 }
 module.exports = { getTreeDirectory };*/
 
-
+const PATH = require('path')
 const dirTree = require('directory-tree');
 function getTreeDirectory(){
-  const filteredTree = dirTree('/home/pi/Documents', {exclude:[/node_modules/,/cache/,/bin/,/lib/,/test_*/,/.git\/*/,/\.([\w|\d]*\s*)/]});
-  console.log(filteredTree);
+  //exclude:[/node_modules/,/cache/,/bin/,/lib/,/test_*/,/.git\/*/,/\.([\w|\d]*\s*)/]}
+  const filteredTree = dirTree('/home/pi/Documents',{exclude:[/node_modules/,/.git\/*/]});
+  console.log(filteredTree)
   return filteredTree
 }
 module.exports = { getTreeDirectory };
